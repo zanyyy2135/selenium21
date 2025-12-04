f8a6920ebb4bb5914d87eca1d2f47144f9e00734
zanyyy2135 <vivekanandanarayanan2005@gmail.com>
2025-12-04 02:13:36 -0800

    Add files via upload

---
+++ java.java
@@ -0,0 +1,12 @@
+package learn.automation;
+
+public class java {
+	public static void main(String[]args) {
+		int ageofPerson=30;
+		char logo='t';
+		String name="Videshi,Nithesh,vivek,sahar";
+		boolean value=true;
+		System.out.println(name);
+	}
+
+}
